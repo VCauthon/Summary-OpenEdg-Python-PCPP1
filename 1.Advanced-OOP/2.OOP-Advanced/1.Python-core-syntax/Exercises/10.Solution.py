ec603739bf1ba2fdeea926abf4cc8b4003fd72ee
VCauthon <victormasmon@gmail.com>
2023-09-09 23:35:22 +0200

    Study 1.2.1 + Exercise 1

---
+++ 1.Advanced-OOP/2.OOP-Advanced/1.Python-core-syntax/Exercises/10.Solution.py
@@ -0,0 +1,53 @@
+class Timer:
+    def __init__(self, hours: int, minutes: int, seconds: int) -> None:
+        self.hours = self.__sanity_check_type(hours, int)
+        self.minutes = self.__sanity_check_type(minutes, int)
+        self.seconds = self.__sanity_check_type(seconds, int)
+
+    def __add__(self, val: 'Timer') -> 'Timer':
+        self.__sanity_check_type(val, Timer)
+
+        # Sums the received timer with its own
+        return self.__seconds_to_timer(
+            self.__timer_to_seconds(self) + self.__timer_to_seconds(val))
+   
+    def __sub__(self, val: 'Timer') -> 'Timer':
+        self.__sanity_check_type(val, Timer)
+
+        # Subtract the received timer with its own
+        return self.__seconds_to_timer(
+            self.__timer_to_seconds(self) - self.__timer_to_seconds(val))
+
+    def __mul__(self, val: int):
+        self.__sanity_check_type(val, int)
+
+        # Multiply the received int by the total of seconds
+        return self.__seconds_to_timer(self.__timer_to_seconds(self) * val)
+
+    @staticmethod
+    def __sanity_check_type(arg, datatype):
+        if not isinstance(arg, datatype):
+            raise TypeError('Unexpected type received for this method')
+        return arg
+
+    @staticmethod
+    def __timer_to_seconds(obj_timer: 'Timer') -> int:
+        return (obj_timer.hours * 3600) + (obj_timer.minutes * 60) + obj_timer.seconds
+
+    @staticmethod
+    def __seconds_to_timer(seconds: int) -> 'Timer':
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return Timer(hours, minutes, seconds)
+
+    def __str__(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+# Creation of the object
+time_object_1 = Timer(1, 50, 1)
+time_object_2 = Timer(20, 51, 1)
+
+# Calling the magic methods
+print(f"Example Sum Timer: {time_object_1 + time_object_2}")
+print(f"Example Sub Timer: {time_object_1 - time_object_2}")
+print(f"Example Mul Timer: {time_object_1 * 2}")
