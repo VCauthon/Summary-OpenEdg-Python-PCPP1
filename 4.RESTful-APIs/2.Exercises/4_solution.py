3d85b55b1b4c2a4098ff2cbb71d69ebfdd88b1c5
Victor Mas <victormasmon@gmail.com>
2024-04-21 10:54:42 +0200

    addrequests 4_solution.py

--- 4.RESTful-APIs/2.Exercises/4_solution.py
+++ 4.RESTful-APIs/2.Exercises/4_solution.py
@@ -1,16 +1,16 @@
 from typing import Optional
-import request
+import requests
+import sys
 
-
-def HTTP_message(http_type: str, direction: str):
+def HTTP_message(http_type: str, direction: str, port: int):
+    url = f"http://{direction}:{port}"
     if http_type.lower() == "get":
-        return b"GET / HTTP/1.1\r\nHost: " + bytes(direction, "utf8") + b"\r\nConnection: close\r\n\r\n"
+        response = requests.get(url)
     elif http_type.lower() == "head":
-        return b"HEAD / HTTP/1.1\r\nHost: " + bytes(direction, "utf8") + b"\r\nConnection: close\r\n\r\n"
-
+        response = requests.head(url)
+    return response
 
 def fake_cli(*args):
-
     # Add each argument into the sys argv
     [sys.argv.append(str(a)) for a in args]
 
@@ -22,20 +22,15 @@ def fake_cli(*args):
     port: int = parse_port(sys.argv[2] if len(sys.argv) > 2 else None)
 
     try:
-        obj_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        obj_socket.connect((ip, port))
+        response = HTTP_message("head", ip, port)
+        print(response.headers)
+        print(response.status_code)
+        print(response.text)
 
-        obj_socket.send(HTTP_message("head", ip))
-        print(obj_socket.recv(10000))
-
-        obj_socket.shutdown(socket.SHUT_RDWR)
-        obj_socket.close()
-
-    except socket.timeout:
+    except requests.exceptions.Timeout:
         error_raiser(3)
-    except socket.gaierror:
+    except requests.exceptions.RequestException:
         error_raiser(4)
-
 
 def error_raiser(error_code: int):
     if error_code in [1, 2, 3, 4]:
@@ -51,28 +46,21 @@ def error_raiser(error_code: int):
             print("There has been an unhandled error during the request")
         sys.exit(error_code)
 
-
 def parse_port(port: str) -> Optional[int]:
-
     # Parsing the received value into an int
     if isinstance(port, str):
         try:
             port = int(port)
-
             # Check that the port number is valid
             if port < 1 or 65535 < port:
                 error_raiser(2)
-
             return port
-
         except ValueError:
             error_raiser(2)
-
     # If there are no parsing return the default HTTP port
-    else :
+    else:
         return 80
-
 
 # CLI simulator
 if __name__ == "__main__":
-    fake_cli("www.testingmcafeesites.com")
+    fake_cli("www.example.com", "80")
