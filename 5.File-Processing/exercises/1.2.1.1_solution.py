55e7ea9cdad9127d9aee88f2248eaaa074c3a0f5
Victor Mas Montserrat <victormasmon@gmail.com>
2024-05-10 18:28:11 +0200

    solution 5.2.1.1

---
+++ 5.File-Processing/exercises/1.2.1.1_solution.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+import sqlite3
+from typing import List, Tuple
+
+
+@dataclass
+class Task:
+    id: int
+    name: str
+    priority: int
+
+# TODO: Empty task can't be added
+# TODO: Task lesser than 0 can't be added
+# TODO: Add decorator of autocommit ?
+# TODO: If you add the same register again
+
+class TODO:
+
+    def __init__(self) -> None:
+        self.session = sqlite3.connect(':memory:')
+        self.cursor = self.session.cursor()
+        self.__set_table()
+
+    def __set_table(self):
+        self.cursor.execute(
+            'CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT NOT NULL, priority INTEGER NOT NULL);')
+        self.session.commit()
+
+    def enter_task(self, todo: str, priority: int) -> None:
+        if not todo or not priority:
+            print("The method that creates function hasn't received all the expected arguments")
+            return False
+        else:
+            # Updates the priority when task already exist
+            if id := self.find_task(todo):
+                self.cursor.execute(f'UPDATE tasks SET priority = {priority} WHERE id = {id[0].id}')
+            # Inserts a new task
+            else:
+                self.cursor.execute('INSERT INTO tasks (name, priority) VALUES (?, ?)', (todo, priority))
+
+            self.session.commit()
+            return True
+
+    def find_task(self, todo: str) -> Task:
+        results = self.cursor.execute(f'SELECT id, name, priority FROM tasks WHERE name = "{todo}";')
+        return [Task(*task) for task in results.fetchall()]
+
+    def show_task(self) -> List[Task]:
+        results = self.cursor.execute('SELECT id, name, priority FROM tasks;')
+        return [Task(*task) for task in results.fetchall()]
+
+
+task_added = [
+    ("My first task", 1),
+    ("My second task", 2),
+    ("My first task", 0),
+    ("", 1),
+    ("My first task", 3)
+]
+
+task_manager = TODO()
+
+for task in task_added:
+    if task_manager.enter_task(*task):
+        print(task_manager.show_task())
