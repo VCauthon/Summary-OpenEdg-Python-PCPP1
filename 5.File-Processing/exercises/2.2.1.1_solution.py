4eec92cfef13533e2d4b4c1179974c78d4a4a4fe
Victor Mas Montserrat <victormasmon@gmail.com>
2024-05-20 16:41:29 +0200

    exercise 5.2.2.1.1

---
+++ 5.File-Processing/exercises/2.2.1.1_solution.py
@@ -0,0 +1,32 @@
+from typing import Iterator, Tuple
+import xml.etree.ElementTree as ET
+from xml.etree.cElementTree import Element
+
+
+class TemperatureConverter:
+    @staticmethod
+    def convert_celsius_to_fahrenheit(celsius: int):
+        return round(9/5 * celsius + 32, 1)
+
+
+class ForecastXmlParser:
+    def __init__(self, path_xml: str) -> None:
+        self.root = ET.parse(path_xml).getroot()
+
+    def parse(self) -> Iterator[str]:
+        for sel in self.root:
+            day, celsius = self.__get_day_properties(sel)
+            yield f"{day}: {celsius} Celsius, {TemperatureConverter.convert_celsius_to_fahrenheit(celsius)} Fahrenheit"
+
+    @staticmethod
+    def __get_day_properties(curr_day: Element) -> Tuple[str, int]:
+        day = curr_day.find("day").text
+        celsius = int(curr_day.find("temperature_in_celsius").text)
+        return day, celsius
+
+
+if __name__ == "__main__":
+    forecast = ForecastXmlParser("5.File-Processing/exercises/forecast.xml")
+
+    for day in forecast.parse():
+        print(day)
